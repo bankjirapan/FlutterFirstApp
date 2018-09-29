6da92124064c401cd0470921e46d4ed1d0e7104f
9banker <jirapan_yankhan@hotmail.com>
2018-09-29 22:40:31 +0700

    [ EDIT ] add overide

--- flutterfirstapp/lib/main.dart
+++ flutterfirstapp/lib/main.dart
@@ -4,6 +4,8 @@ void main() => runApp(MyFirstApp());
 
 
 class MyFirstApp extends StatelessWidget {
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
