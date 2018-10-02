64275b496fbdc076954acdcb1dd3abcedfb544b7
9banker <jirapan_yankhan@hotmail.com>
2018-10-03 00:50:06 +0700

    [ EDIT ] Add function state

--- flutterfirstapp/lib/main.dart
+++ flutterfirstapp/lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyFirstApp());
 
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
 
-class MyFirstApp extends StatelessWidget {
+    //TODO : implements create state
+    return _MyFirstAppState();
+  }
+}
 
+class _MyFirstAppState extends State<MyFirstApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,12 +19,25 @@ class MyFirstApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("MyFirstApp"),
         ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text("Food paradine")
-           
-        ],),),
-        
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(5.0),
+              child: RaisedButton(
+                child: Text('Add Product'),
+                onPressed: () {},
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text("Food paradine")
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
