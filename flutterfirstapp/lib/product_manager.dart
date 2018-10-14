7f82fdb26c904873c43d10ce1d1fa84e8d1eb2e2
9banker <jirapan_yankhan@hotmail.com>
2018-10-15 00:37:53 +0700

    [ EDIT ] Add product Manager

--- flutterfirstapp/lib/product_manager.dart
+++ flutterfirstapp/lib/product_manager.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import './products.dart';
 
 class ProductManager extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return null;
+    return _productManagerState();
   }
 }
 
 class _productManagerState extends State<ProductManager> {
+  List<String> _products = ["Food Tester"];
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Column(children :  [Container(
+      margin: EdgeInsets.all(5.0),
+      child: RaisedButton(
+        child: Text('Add Product'),
+        onPressed: () {
+          setState(() {
+            _products.add('Advanced Food Tester');
+            print(_products);
+          });
+        },
+      ),
+    ),
+    Products(_products)
+    ]);
   }
 }
