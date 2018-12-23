8dc82ce65640345b3e0a4ec8fa2be3cd1fe6e051
llUJO <jirapan_yankhan@hotmail.com>
2018-12-23 17:57:05 +0700

    [ ADD ] product control

---
+++ firstapp/lib/productControl.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProductControl extends StatelessWidget {
+
+  final Function addProduct;
+
+  ProductControl(this.addProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RaisedButton(
+      textColor: Colors.white,
+      color: Colors.blueGrey,
+      onPressed: () {
+        addProduct("TomYumKrung");
+      },
+      child: Text("Add Product"),
+    );
+  }
+}
